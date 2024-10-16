02c3f001848c86f8b3d4ee6aecf859952a732d9c
Abdulazizkhan777 <abdulazizkhan107925@gmail.com>
2024-10-16 21:49:39 +0500

    commit

---
+++ Ali-Portfolio-main/src/components/Projects/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Research & Projects</Title>
+        <Desc>
+          Here are details of different research papers and projects which I have been working on.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ Ali-Portfolio-main/src/data/constants.js
@@ -0,0 +1,295 @@
+export const Bio = {
+  name: "Muhammad Ali Hassan",
+  roles: [
+    "Cybersecurity Student",
+    "Cybersecurity Specialist",
+    "Penetration Tester",
+    "Digital Forensics Analyst",
+    "Network Security Engineer",
+    "Security Software Developer",
+    "Cybercrime Investigator",
+    "AI Security Engineer",
+    "Secure Systems Architect",
+    "Cybersecurity Researcher"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. A humble individual with a strong will who believes in discipline to achieve anything he sets his mind to. A technophile who is always curious about the next big breakthrough in technology",
+  github: "https://github.com/aleeh7n",
+  resume:
+    "https://drive.google.com/file/d/1Z2ckLpwkYcMCc9A3UiOfqUzVl1Qt-I1s/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/aleeh7n/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/p/DBEi3O7uJjJ/?igsh=bTZkMXphdzFpN2sy",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Technical Skills",
+    skills: [
+      {
+        name: "Linux Scripts & Shells",
+        image:
+          "https://w7.pngwing.com/pngs/48/567/png-transparent-bash-shell-script-command-line-interface-z-shell-shell-rectangle-logo-commandline-interface-thumbnail.png",
+      },
+      {
+        name: "Wireshark",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Wireshark_icon.svg/1200px-Wireshark_icon.svg.png",
+      },
+      {
+        name: "Nessus",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBbV0OuU6ypffziHg2CDB0Rn9f1XJBT6uUEQ&s",
+      },
+      {
+        name: "OWASP ZAP",
+        image: "https://e7.pngegg.com/pngimages/170/187/png-clipart-owasp-zap-vulnerability-penetration-test-proxy-server-web-application-miscellaneous-angle.png",
+      },
+      {
+        name: "Burp Suite",
+        image:
+          "https://miro.medium.com/v2/resize:fit:710/0*FvyoEolATs1TVCy9.png",
+      },
+      {
+        name: "OpenVAS",
+        image:
+          "https://forum.greenbone.net/uploads/default/original/1X/45982934a730d1824bc403f78875b77a8dfc1ae0.png",
+      },
+      {
+        name: "Nexpose",
+        image:
+          "https://www.wavetel.fr/wp-content/uploads/2015/12/RAPID7-NEXPOSE-Gestion-des-vulnerabilites.png",
+      },
+      {
+        name: "Autopsy",
+        image:
+          "https://www.gohtci.com/wp-content/uploads/2019/05/autopsy-900x.jpg",
+      },
+      {
+        name: "Digital Marketing",
+        image:
+          "https://static3.bigstockphoto.com/7/0/3/large1500/30797855.jpg",
+      },
+    ],
+  },
+  {
+    title: "Generic Skills",
+    skills: [
+      {
+        name: "Information Security",
+        image: "https://cdn.vectorstock.com/i/500p/97/67/cyber-security-logo-template-vector-20409767.jpg",
+      },
+      {
+        name: "Penetration Testing",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/10961/10961506.png",
+      },
+      {
+        name: "Vulnerability Assessment & Reverse Engineering",
+        image: "https://media.licdn.com/dms/image/v2/C5112AQETbWf_VwdM_Q/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1551884906672?e=2147483647&v=beta&t=j2RZLYrjgycNEphEWToUPxyNHcs42xVtdbQbvuYN6W8",
+      },
+      {
+        name: "Digital Forensics",
+        image:
+          "https://w7.pngwing.com/pngs/32/739/png-transparent-forensic-science-digital-forensics-computer-forensics-criminal-investigation-crime-binary-code-s-text-computer-monochrome-thumbnail.png",
+      },
+      {
+        name: "Network Forensics",
+        image:
+          "https://iwmcybersec.com/wp-content/uploads/2021/06/network_forensics.png",
+      },
+      {
+        name: "Cybersecurity Analysis",
+        image:
+          "https://www.shutterstock.com/image-vector/abstract-cyber-security-logo-line-260nw-2050870868.jpg",
+      },
+     
+    ],
+  },
+  {
+    title: "Soft Skills",
+    skills: [
+      {
+        name: "Leadership",
+        image:
+          "https://st2.depositphotos.com/1768926/10264/v/450/depositphotos_102644204-stock-illustration-community-care-logo-template.jpg",
+      },
+      {
+        name: "Communication",
+        image:
+          "https://e7.pngegg.com/pngimages/441/361/png-clipart-communication-logo-organization-company-public-relations-communication-miscellaneous-company.png",
+      },
+      {
+        name: "Teamwork",
+        image:
+          "https://w7.pngwing.com/pngs/52/969/png-transparent-logo-graphy-teamwork-community-miscellaneous-purple-violet-thumbnail.png",
+      },
+      {
+        name: "Project Management",
+        image:
+          "https://e7.pngegg.com/pngimages/408/212/png-clipart-project-management-body-of-knowledge-project-management-professional-project-manager-management-project-miscellaneous-text-thumbnail.png",
+      },
+      {
+        name: "Attention to Details",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/13502/13502789.png",
+      },
+      {
+        name: "Critical thinking",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/9631/9631082.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Voluntary Work (100+ hours of community service)",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyo6RzmoJml9ZHpdejvEXQiRA-9S2JLbK_PQ&s",
+      },
+      {
+        name: "Horse Ridding",
+        image:
+          "https://logowik.com/content/uploads/images/horse-rider3541.logowik.com.webp",
+      },
+      {
+        name: "Fitness",
+        image:
+          "https://marketplace.canva.com/EAFMWRbCRm0/1/0/1600w/canva-red-black-minimalist-fitness-logo-dcYVFBq4DTg.jpg",
+      },
+      {
+        name: "Basketball",
+        image:
+          "https://png.pngtree.com/png-vector/20191027/ourmid/pngtree-basketball-championship-logo-modern-professional-basketball-logo-design-png-image_1855709.jpg",
+      },
+      {
+        name: "Cyber Security News",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoFhgkYn0940GINNtZ-FpEISwbJd0CW0HRuQ&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQEHp9PBbXWstw/company-logo_200_200/company-logo_200_200/0/1660033288684/cytomate_logo?e=2147483647&v=beta&t=xqCChlftq0PpFqlDDYecnIMKXqwu6OyMFk375Jxftm0",
+    role: "Information Security Specialist",
+    company: "Cytomate",
+    date: "March 2023 - Present",
+    desc: "As an Information Security Specialist at Cytomate, Doha, I have developed Phish Avenger, a browser extension using machine learning to detect phishing attempts. I made improved email content analysis and URL inspection algorithms and conducted research on anti-phishing tools, contributing to educational materials to raise awareness of email security risks.",
+    skills: [
+      "Information Security Specialist",
+      "Machine Learning Engineer (Security)",
+      "Phishing Detection Analyst",
+      "Security Software Developer",
+      "Security Researcher",
+      "Security Awareness Trainer",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://moitt.gov.pk/SiteImage/Setting/gov_logo.webp",
+    role: "Digital Marketing Intern",
+    company: "Ignite; Ministry of information and Technology",
+    date: "oct 2020 - jan 2021",
+    desc: "As a Digital Marketing Intern at Ignite and the Ministry of IT & Telecom, Lahore, you implemented SEO strategies, ran advertising campaigns, and gained hands-on experience in various digital marketing areas, including ad optimization.",
+    skills: [
+      "ReactJS SEO Specialist",
+      "Digital Marketing Analyst",
+      "Advertising Campaign Manager",
+      "Social Media Marketer",
+      "Content Marketing Specialist",
+      "PPC (Pay-Per-Click) Specialist",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBWjxuZx__a25hgj1JCqKkQF-6_UiqrUG7_g&s",
+    role: "Student Ambassador",
+    company: "Microsoft",
+    date: "Nov 2021 - Present ",
+    desc: "I have served as member of the Microsoft learn Student Ambassadors think-tank in the AU Chapter; I co-ordinated two successful annual coding competitions-Code Ai 1 in 2022 and Code Air 2 in 2023-that included design of coding challenges as well as judging submitted entries",
+    skills: [
+      "Leadership",
+      "Communication",
+      "Teamwork",
+      "Material UI",
+      "Project Management",
+      "Attention to Details",
+      "Analytical Thinking",
+      "Critical Thinking",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/v2/D4D0BAQGj5f63f_Bllg/company-logo_100_100/company-logo_100_100/0/1666001595896/akhuwat_logo?e=1736985600&v=beta&t=kUf3K3tkOeotPmVFMzU_fX41tEm3FOA2cow2QrRlBbI",
+    role: "Community Service (contract) ",
+    company: "Akhuwat",
+    date: "May 2017 - May 2019",
+    desc: "Providing and helping the down-trodden community",
+    
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQGS5JjUz-DNzA/company-logo_100_100/company-logo_100_100/0/1709514716670/wwf_international_logo?e=1736985600&v=beta&t=Fb47zEyGt7jirhpaWwkaeLx3-VBtav8L-bOlhaMrAFM",
+    role: "Community Service",
+    company: "WWF",
+    date: "Apr 2017 - Apr 2017",
+    desc: "A week full of environment friendly activities",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQEqvdCZo8JFJg/company-logo_100_100/company-logo_100_100/0/1704434874496/air_university_logo?e=1736985600&v=beta&t=kJ837igK58RK7mr2VNttxE0HU9syUzaR5apgIEAhjXU",
+    school: "Air University",
+    date: "Aug 2021 - Aug 2025",
+    grade: "A",
+    desc: "As a Cybersecurity student at Air University, I am  gaining a strong foundation in information security, threat analysis, network security, and ethical hacking, with hands-on experience in modern cybersecurity tools and techniques. My coursework and practical projects prepare you to address evolving cyber threats and protect digital infrastructures.",
+    degree: "BS Cybersecurity",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/C510BAQHBw5xVZzU_tA/company-logo_100_100/company-logo_100_100/0/1630578020661/cadet_college_hasanabdal1954_logo?e=1736985600&v=beta&t=H_pOS1HXdcDHgbBWodwaveEtjFiLwirskIxcT2tqW0M",
+    school: "Cadet College Hasanabdal",
+    date: "Apr 2014 - May 2020",
+    grade: "A",
+    desc: "Activities & Description: President Calligraphy Club | Captain College Gymnastics Team (Color) | Member College Hockey Team (Color) | Best Shooter Obstacles (2015) | Community Service [Akhuwat (53 hours), Al-Khidmat (20 hours), WWF (18 hours)] | Activities and societies: President Calligraphy Club Captain College Gymnastics Team (Color) Member College Hockey Team (Color) Best Shooter Obstacles (2015) Community Service [Akhuwat (53 hours), Al-Khidmat (20 hours), WWF (18 hours)] Learned Discipline, Time-management, Leadership and Communication skills at CCH.",
+    degree: "Intermediate, Pre-Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Information Security",
+    description:
+      "Currently carrying out research on the field of Information Security with focus on Cognitive Biases and Heuristics in Information Security Decision Making.",
+      image:
+      "https://resourcecenter.infinit-o.com/wp-content/uploads/2023/04/Blographics-The-5-Pillars-of-Informations.-11072019_1.webp",
+  },
+  {
+    id: 0,
+    title: "Anti-phishing Project",
+    description:
+      "Currently working with Cytomate on developing an advanced anti-phishing tool known as Phishing Avenger based on machine learning to improve security and user training",
+    image:
+      "https://www.idagent.com/wp-content/uploads/2024/09/choose-AI-antiphishing.jpg",
+    
+  },
+ 
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
